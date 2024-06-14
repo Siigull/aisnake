8909b9b21b7c6365131e5bfdb7a0db2ae4c621b9
xpeland00 <dan@Daniels-MacBook-Air-2.local>
2024-06-14 17:02:07 +0200

    majsnejk

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from random import randint
 pg.init()
 font = pg.font.SysFont('dejavusansmono', 25)
 
-Window_Height = 1000
-Window_Width = 1000
-screen = pg.display.set_mode((Window_Width, Window_Height))
+Window_Height_pixels = 1000
+Window_Width_pixels = 1000
 Tile_Size = 50
-clock = pg.time.Clock()
-random_range = (Tile_Size, (Window_Width // Tile_Size) - Tile_Size)
-get_random_position = lambda: [randrange(1, (Window_Width // Tile_Size)) * Tile_Size, randrange(1, (Window_Height // Tile_Size)) * Tile_Size]
+Window_Height = Window_Height_pixels // Tile_Size
+Window_Width = Window_Width_pixels // Tile_Size
 
+screen = pg.display.set_mode((Window_Width_pixels, Window_Height_pixels))
+
+clock = pg.time.Clock()
+get_random_position = lambda: [randrange(1, (Window_Width_pixels // Tile_Size)) * Tile_Size, randrange(1, (Window_Height_pixels // Tile_Size)) * Tile_Size]
 
 # reset 
 # reward 
@@ -19,7 +21,8 @@ get_random_position = lambda: [randrange(1, (Window_Width // Tile_Size)) * Tile_
 # frame, state, game iteration 
 # is collision change
 
-
+def random_position():
+    return (randrange(1, Window_Width - 1), randrange(1, Window_Height - 1))
 
 class SnakeAgent:
     def __init__(self):
@@ -32,33 +35,22 @@ class SnakeAgent:
         self.score = 0
 
     def random_head_position(self):
-        self.head.append(((randrange(100, Window_Width, Tile_Size), randrange(100, Window_Height, Tile_Size))))
+        self.head.append(random_position())
         self.positions = self.head
 
     def move(self):
-        snake_len = len(self.positions) - 1
-       
-        self.lastpositionbuff = self.positions[snake_len]
-        del self.positions[snake_len] 
+        self.lastpositionbuff = self.positions[-1]
 
-        if snake_len == 0:
-            if self.direction == 0:
-                self.positions.insert(0, (self.lastpositionbuff[0], self.lastpositionbuff[1] - Tile_Size))
-            if self.direction == 1:
-                self.positions.insert(0, (self.lastpositionbuff[0] + Tile_Size, self.lastpositionbuff[1]))
-            if self.direction == 2:
-                self.positions.insert(0, (self.lastpositionbuff[0], self.lastpositionbuff[1] + Tile_Size))
-            if self.direction == 3:
-                self.positions.insert(0, (self.lastpositionbuff[0] - Tile_Size, self.lastpositionbuff[1]))
-        else:
-            if self.direction == 0:
-                self.positions.insert(0, (self.positions[0][0], self.positions[0][1] - Tile_Size))
-            if self.direction == 1:
-                self.positions.insert(0, (self.positions[0][0] + Tile_Size, self.positions[0][1]))
-            if self.direction == 2:
-                self.positions.insert(0, (self.positions[0][0], self.positions[0][1] + Tile_Size))
-            if self.direction == 3:
-                self.positions.insert(0, (self.positions[0][0] - Tile_Size, self.positions[0][1]))
+        if self.direction == 0:
+            self.positions.insert(0, (self.positions[0][0], self.positions[0][1] - 1))
+        if self.direction == 1:
+            self.positions.insert(0, (self.positions[0][0] + 1, self.positions[0][1]))
+        if self.direction == 2:
+            self.positions.insert(0, (self.positions[0][0], self.positions[0][1] + 1))
+        if self.direction == 3:
+            self.positions.insert(0, (self.positions[0][0] - 1, self.positions[0][1]))
+
+        del self.positions[-1]
         
     def reset(self):
         self.length = 1
@@ -77,14 +69,11 @@ class Food:
     def __init__(self):
         self.position = []
 
-    def randomize_position(self):
-        self.position.append(((randrange(0, Window_Width, Tile_Size), randrange(0, Window_Height, Tile_Size))))
-        for i in self.position:
-            if i == 0 or i == 25:
-                i = 50
+    def add(self):
+        self.position.append(random_position())
 
     def draw(self):
-        pg.draw.rect(screen, 'red', (self.position[0], self.position[1], Tile_Size, Tile_Size))
+        pg.draw.rect(screen, 'red', (self.position[0] * Tile_Size, self.position[1] * Tile_Size, Tile_Size, Tile_Size))
         
 
 def check_collision(snake, food):
@@ -92,16 +81,15 @@ def check_collision(snake, food):
         snake.length += 1
         snake.positions.append(snake.lastpositionbuff)
         food.position = []
-        food.randomize_position()
+        food.add()
         snake.score += 1
         snake.drawScore()
-    if snake.positions[0][0] == Window_Width or snake.positions[0][0] == -Tile_Size:
-        print(snake.positions[0][0])
+
+    if snake.positions[0][0] == Window_Width or snake.positions[0][0] == -1:
         print('Game Over')
-        pg.quit()
-        quit()
-    if snake.positions[0][1] == Window_Height or snake.positions[0][1] == -Tile_Size:
-        print(snake.positions[0][1])
+        game_reset(snake, food)
+
+    if snake.positions[0][1] == Window_Height or snake.positions[0][1] == -1:
         print('Game Over')
         game_reset(snake, food)
         
@@ -109,69 +97,56 @@ def check_collision(snake, food):
         for i in snake.positions[1:]:
             if snake.positions[0] == i:
                 print('Game Over')
-                pg.quit()
-                quit()
+                game_reset(snake, food)
         
 
 def change_direction(snake, event, food):
     for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                quit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_UP:
-                    snake.direction = 0
-                if event.key == pg.K_RIGHT:
-                    snake.direction = 1
-                if event.key == pg.K_DOWN:
-                    snake.direction = 2
-                if event.key == pg.K_LEFT:
-                    snake.direction = 3
-                if event.key == pg.K_w:
-                    snake.direction = 0
-                if event.key == pg.K_d:
-                    snake.direction = 1
-                if event.key == pg.K_s:
-                    snake.direction = 2
-                if event.key == pg.K_a:
-                    snake.direction = 3
+        if event.type == pg.QUIT:
+            pg.quit()
+            quit()
 
-    draw_grid(snake, food)
+        if event.type == pg.KEYDOWN:
+            if (event.key == pg.K_UP or event.key == pg.K_w) and snake.direction != 2:
+                snake.direction = 0
+            if (event.key == pg.K_RIGHT or event.key == pg.K_d) and snake.direction != 3:
+                snake.direction = 1
+            if (event.key == pg.K_DOWN or event.key == pg.K_s) and snake.direction != 0:
+                snake.direction = 2
+            if (event.key == pg.K_LEFT or event.key == pg.K_a) and snake.direction != 1:
+                snake.direction = 3
+
     snake.move()
     draw_grid(snake, food)
     check_collision(snake, food)
 
 def draw_grid(snake, food):
     screen.fill((0, 0, 0))
-    counter_head = 0
-    for x, y in snake.positions:
-            print(snake.positions)
-            if counter_head == 0:
-                [pg.draw.rect(screen, 'green', (x, y, Tile_Size, Tile_Size))]
-                counter_head += 1
-            else:
-                [pg.draw.rect(screen, 'blue', (x, y, Tile_Size, Tile_Size))]
 
-    [pg.draw.rect(screen, 'red', (x, y, Tile_Size, Tile_Size)) for x, y in food.position]
+    [pg.draw.rect(screen, 'green', (snake.positions[0][0] * Tile_Size, 
+                                    snake.positions[0][1] * Tile_Size, 
+                                    Tile_Size, Tile_Size))]
 
+    for x, y in snake.positions[1:]:
+        [pg.draw.rect(screen, 'blue', (x * Tile_Size, y * Tile_Size, Tile_Size, Tile_Size))]
+
+    [pg.draw.rect(screen, 'red', (x * Tile_Size, y * Tile_Size, Tile_Size, Tile_Size)) for x, y in food.position]
 
 
-    
 def game_reset(snake, food):
     snake.reset()
     food.position = []
-    food.randomize_position()
+    food.add()
     snake.drawScore()
     draw_grid(snake, food)
     pg.display.flip()
-
 
 def main():
     snake = SnakeAgent()
     SnakeAgent.random_head_position(snake)
     food = Food()
     snake.drawScore()
-    Food.randomize_position(food)
+    Food.add(food)
     
     while True: 
         change_direction(snake, pg.event, food)
